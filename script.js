cf9802d5dfa46098aa74113a786707e699dfc7b1
kmiono <86765332+kmiono@users.noreply.github.com>
2023-09-06 17:04:55 +0900

    作業途中

--- script.js
+++ script.js
@@ -22,4 +22,32 @@ const items = [
   { name: 'penguin', image: 'penguin.png' },
   { name: 'syakutorimusi', image: 'syakutorimusi.png' },
   { name: 'taka', image: 'taka.png' },
+  { name: 'yagi', image: 'yagi.png' },
+  { name: 'azarasi', image: 'azarasi.png' },
 ];
+
+// Initial Time
+let seconds = 0,
+  minutes = 0;
+
+// Initial moves and win count
+let movesCount = 0,
+  winCount;
+
+// For Timer
+const timeGenerator = () => {
+  seconds += 1;
+  // minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+
+  // format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+// For calculating moves
