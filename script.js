4c825c6b358d516fb2e5b33c2fc222033ef469a5
kmiono <86765332+kmiono@users.noreply.github.com>
2023-09-07 16:40:48 +0900

    初期化まで設定、画像表示

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const moves = document.getElementById('moves-count');
 const timeValue = document.getElementById('time');
 const startButton = document.getElementById('start');
 const stpoButton = document.getElementById('stop');
-const gemeContainer = document.querySelector('.game-container');
+const gameContainer = document.querySelector('.game-container');
 const result = document.getElementById('result');
 const controls = document.querySelector('.controls-container');
 let cards;
@@ -51,3 +51,59 @@ const timeGenerator = () => {
 };
 
 // For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+// Pick ramdom objects from item array
+const generateRandom = (size = 4) => {
+  // temporary array
+  let tempArray = [...items];
+  // initializes cardValues array
+  let cardValues = [];
+  // size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size = (size * size) / 2;
+  // Random object selection
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    // once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+  gameContainer.innerHTML = '';
+  cardValues = [...cardValues, ...cardValues];
+  // simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
+    /*
+    Create cards
+    before => front side (contains puestion mark)
+    after => back side (containts actual image)
+    data-card-values is a custom attribute which stores the names of the cards to match later
+    */
+    gameContainer.innerHTML += `
+   <div class="card-container" data-card-value="${cardValues[i].name}">
+   <div class="card-before">?</div>
+   <div class="card-after"><img src="${cardValues[i].image}" class="image" width="75" height="75"/></div>
+   </div>   
+   `;
+  }
+  // Grid
+  gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+};
+
+// Initialized values and func calls
+const initializer = () => {
+  result.innerText = '';
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
+
+initializer();
